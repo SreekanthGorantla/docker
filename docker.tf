444369d18e08c2af7f3695161a86da68a8a88f7c
Sreekanth Gorantla <gorantlasreekanth@gmail.com>
2025-07-11 13:11:21 -0500

    docker commit changes 1

--- docker.tf
+++ docker.tf
@@ -28,6 +28,18 @@ resource "aws_security_group" "allow_tls" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
