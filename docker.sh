333a519f6853f5ea435b46a8467d30880c1fc23f
Sreekanth Gorantla <gorantlasreekanth@gmail.com>
2025-07-18 15:29:43 -0500

    docker helm installation commit

--- docker.sh
+++ docker.sh
@@ -73,3 +73,9 @@ echo "move eksctl to /usr/local/bin..."
 mv /tmp/eksctl /usr/local/bin
 
 echo "eksctl installation completed successfully!"
+
+# Installing helm
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
